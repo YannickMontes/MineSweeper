053c2523cd55b26f29a446ea407ee303e9888eb5
MONTES Yannick <montes.yannick@gmail.com>
2016-06-15 20:02:21 +0200

    Added a class for only number textfields.

---
+++ src/minesweeper_view/NumberTextField.java
@@ -0,0 +1,34 @@
+package minesweeper_view;
+
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author yannick
+ */
+public class NumberTextField extends TextField
+{
+
+    @Override
+    public void replaceText(int start, int end, String text)
+    {
+        if (validate(text))
+        {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text)
+    {
+        if (validate(text))
+        {
+            super.replaceSelection(text);
+        }
+    }
+
+    private boolean validate(String text)
+    {
+        return text.matches("[0-9]*");
+    }
+}
